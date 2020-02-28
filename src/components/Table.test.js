a223d360f6df3ad1bf0892326694ec138d127bb8
maku <manojasho@gmail.com>
2020-02-28 17:10:15 +0000

    Refactor test to use default click method

--- src/components/Table.test.js
+++ src/components/Table.test.js
@@ -1,94 +1,70 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import Table from "./Table";
-import { wait } from "@testing-library/dom";
 
-test("Should render a data table with column headings", () => {
-  const { getByText } = render(<Table />);
+describe("Table", () => {
+  test("Should render a data table with column headings", () => {
+    const { getByText } = render(<Table />);
 
-  expect(getByText("Symbol")).toBeInTheDocument();
-  expect(getByText("Gene ID")).toBeInTheDocument();
-  expect(getByText("Gene Name")).toBeInTheDocument();
-  expect(getByText("Overall Association Score")).toBeInTheDocument();
+    expect(getByText("Symbol")).toBeInTheDocument();
+    expect(getByText("Gene ID")).toBeInTheDocument();
+    expect(getByText("Gene Name")).toBeInTheDocument();
+    expect(getByText("Overall Association Score")).toBeInTheDocument();
+  });
+
+  test("Display data passed in", async () => {
+    const data = [
+      {
+        symbol: "EGFR",
+        geneId: "ENSG0001",
+        geneName: "epidermal growth",
+        overallAssociationScore: 1.0
+      },
+      {
+        symbol: "SMAD2",
+        geneId: "ENSG0002",
+        geneName: "epidermal growth",
+        overallAssociationScore: 1.0
+      }
+    ];
+
+    const { getAllByTestId } = render(<Table data={data} />);
+    const tableRows = getAllByTestId("data-row");
+    const tableData = tableRows[0].querySelectorAll("td");
+
+    expect(tableRows.length).toBe(2);
+    expect(tableData.length).toBe(5);
+  });
+
+  test("Display no rows if no data", () => {
+    const data = [];
+
+    const { container } = render(<Table data={data} />);
+    const tableRows = container.querySelectorAll("tbody>tr");
+
+    expect(tableRows.length).toBe(0);
+  });
+
+  test("Should have column of buttons", () => {
+    const data = [
+      {
+        symbol: "EGFR",
+        geneId: "ENSG0001",
+        geneName: "epidermal growth",
+        overallAssociationScore: 1.0
+      }
+    ];
+
+    const { container } = render(<Table data={data} />);
+    const buttons = container.querySelectorAll("button");
+
+    expect(buttons.length).toBe(1);
+  });
 });
 
-test("Display data passed in", async () => {
-  const data = [
-    {
-      symbol: "EGFR",
-      geneId: "ENSG0001",
-      geneName: "epidermal growth",
-      overallAssociationScore: 1.0
-    },
-    {
-      symbol: "SMAD2",
-      geneId: "ENSG0002",
-      geneName: "epidermal growth",
-      overallAssociationScore: 1.0
-    }
-  ];
+describe("Button behaviour", () => {
+  const visibleRowsSelector = "tr[aria-hidden='false']";
 
-  const { getAllByTestId } = render(<Table data={data} />);
-  const tableRows = getAllByTestId("data-row");
-  const tableData = tableRows[0].querySelectorAll("td");
-
-  expect(tableRows.length).toBe(2);
-  expect(tableData.length).toBe(5);
-});
-
-test("Display no rows if no data", () => {
-  const data = [];
-
-  const { container } = render(<Table data={data} />);
-  const tableRows = container.querySelectorAll("tbody>tr");
-
-  expect(tableRows.length).toBe(0);
-});
-
-test("Should have column of buttons", () => {
-  const data = [
-    {
-      symbol: "EGFR",
-      geneId: "ENSG0001",
-      geneName: "epidermal growth",
-      overallAssociationScore: 1.0
-    }
-  ];
-
-  const { container } = render(<Table data={data} />);
-  const buttons = container.querySelectorAll("button");
-
-  expect(buttons.length).toBe(1);
-});
-
-test("Should display extra row if button is pressed", () => {
-  const data = [
-    {
-      symbol: "EGFR",
-      geneId: "ENSG0001",
-      geneName: "epidermal growth",
-      overallAssociationScore: 1.0
-    }
-  ];
-
-  const { container } = render(<Table data={data} />);
-  const button = container.querySelector("button");
-  let rows = container.querySelectorAll("tr[aria-hidden='true']");
-  expect(rows.length).toBe(1);
-
-  fireEvent(
-    button,
-    new MouseEvent("click", {
-      bubbles: true,
-      cancelable: true
-    })
-  );
-
-  rows = container.querySelectorAll("tr[aria-hidden='false']");
-  expect(rows.length).toBe(1);
-});
-
-test("Should hide extra row if button is pressed again", () => {
   const data = [
     {
       symbol: "EGFR",
@@ -98,29 +74,28 @@ test("Should hide extra row if button is pressed again", () => {
     }
   ];
 
-  const { container } = render(<Table data={data} />);
-  const button = container.querySelector("button");
-  let rows = container.querySelectorAll("tr[aria-hidden='true']");
+  test("Should display extra row if button is pressed", () => {
+    const { container } = render(<Table data={data} />);
+    const button = container.querySelector("button");
+    let visibleRows = container.querySelectorAll(visibleRowsSelector);
+    expect(visibleRows.length).toBe(0);
 
-  fireEvent(
-    button,
-    new MouseEvent("click", {
-      bubbles: true,
-      cancelable: true
-    })
-  );
+    fireEvent.click(button);
 
-  rows = container.querySelectorAll("tr[aria-hidden='false']");
-  expect(rows.length).toBe(1);
+    visibleRows = container.querySelectorAll(visibleRowsSelector);
+    expect(visibleRows.length).toBe(1);
+  });
 
-  fireEvent(
-    button,
-    new MouseEvent("click", {
-      bubbles: true,
-      cancelable: true
-    })
-  );
+  test("Should hide extra row if button is pressed again", () => {
+    const { container } = render(<Table data={data} />);
+    const button = container.querySelector("button");
+    fireEvent.click(button);
+    let visibleRows = container.querySelectorAll(visibleRowsSelector);
+    expect(visibleRows.length).toBe(1);
 
-  rows = container.querySelectorAll("tr[aria-hidden='true']");
-  expect(rows.length).toBe(1);
+    fireEvent.click(button);
+
+    visibleRows = container.querySelectorAll(visibleRowsSelector);
+    expect(visibleRows.length).toBe(0);
+  });
 });
